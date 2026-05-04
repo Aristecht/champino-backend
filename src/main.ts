bde82e09f1026458ddde2d447bed7897616c9afb
Aristarh <aristiktop8@gmail.com>
2026-05-04 15:22:38 +0500

    fix: google auth 2

--- src/main.ts
+++ src/main.ts
@@ -23,9 +23,7 @@ async function bootstrap() {
   const apiPrefix = config.get<string>('API_PREFIX')?.replace(/^\/+|\/+$/g, '');
 
   if (apiPrefix) {
-    app.setGlobalPrefix(apiPrefix, {
-      exclude: ['oauth/(.*)', 'oauth/google', 'oauth/google/callback'],
-    });
+    app.setGlobalPrefix(apiPrefix);
   }
 
   app.use(cookieParser(config.getOrThrow<string>('COOKIE_SECRET')));
