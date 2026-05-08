134feda5ac722e16c377cb2a7f89bdb889778553
Aristarh <aristiktop8@gmail.com>
2026-05-08 11:31:53 +0500

    fix: cron sync rosta info

--- src/modules/cron/cron.service.ts
+++ src/modules/cron/cron.service.ts
@@ -16,7 +16,7 @@ export class CronService {
     private readonly rostaSyncService: RostaSyncService,
   ) {}
 
-  @Cron(CronExpression.EVERY_5_MINUTES)
+  @Cron(CronExpression.EVERY_DAY_AT_3AM)
   async syncRostaProducts() {
     await this.rostaSyncService.syncAll();
   }
