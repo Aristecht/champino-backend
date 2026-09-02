6dcd42ba0a2084032ed986b3e5866a590c87fe2b
Aristarh <aristiktop8@gmail.com>
2026-09-02 10:37:22 +0500

    fix: storage module

--- src/modules/content/news/news.module.ts
+++ src/modules/content/news/news.module.ts
@@ -3,9 +3,11 @@ import { NewsService } from './news.service';
 import { NewsResolver } from './news.resolver';
 import { PrismaModule } from '../../../core/prisma/prisma.module';
 import { NewsImageController } from './news-image.controller';
+import { NotificationsModule } from '../../notifications/notifications.module';
+import { StorageModule } from '../../libs/storage/storage.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, NotificationsModule, StorageModule],
   controllers: [NewsImageController],
   providers: [NewsResolver, NewsService],
 })
